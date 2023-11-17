2e77539f75746f69da653a0016c1f4da745b1743
DinarKhazimullinITICBcn <146452935+DinarKhazimullinITICBcn@users.noreply.github.com>
2023-11-17 16:33:32 +0100

    Creat el controllador amb les dues funcions que redirigeixen a les views corresponents

---
+++ dinar/app/Http/Controllers/SignController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SignController extends Controller
+{
+    //
+    public function signin() {
+        return view('signin');
+    }
+    public function signup() {
+        return view('signup');
+    }
+}
