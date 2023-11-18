938e4c6523d203b268d9dbf05f1b336fbb887e1a
DinarKhazimullinITICBcn <146452935+DinarKhazimullinITICBcn@users.noreply.github.com>
2023-11-18 15:29:10 +0100

    Modificat el controlador per a que se li pasin els parametres y s'hi afegeixin a la view corresponent

--- dinar/app/Http/Controllers/SignController.php
+++ dinar/app/Http/Controllers/SignController.php
@@ -7,10 +7,19 @@ use Illuminate\Http\Request;
 class SignController extends Controller
 {
     //
-    public function signin() {
-        return view('signin');
+    public function signin($valor1, $valor2, $valor3, $valor4) {
+        $textFinal = "";
+        $textFinal .= ($valor1 == "") ? ' Iniciar ': $valor1 . " ";
+        $textFinal .= ($valor2 == "") ? ' sessio ': $valor2 . " ";
+        $textFinal .= ($valor3 == "") ? ' de ': $valor3 . " ";
+        $textFinal .= ($valor4 == "") ? ' l\'usuari': $valor4;
+        return view('signin') ->with('textFinal',$textFinal);
     }
-    public function signup() {
-        return view('signup');
+    public function signup($valor1, $valor2, $valor3) {
+        $textFinal = "";
+        $textFinal .= ($valor1 == "") ? ' Creacio ': $valor1 . " ";
+        $textFinal .= ($valor2 == "") ? ' d\'usuari ': $valor2 . " ";
+        $textFinal .= ($valor3 == "") ? ' nou ': $valor3 . " ";
+        return view('signup') ->with('textFinal', $textFinal);
     }
 }
