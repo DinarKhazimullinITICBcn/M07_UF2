8c0abad17df9437f9bbdc117cdea4a24d441f6a6
DinarKhazimullinITICBcn <146452935+DinarKhazimullinITICBcn@users.noreply.github.com>
2023-11-24 16:35:37 +0100

    Afegit productes de la diapositiva

--- dinar/app/Http/Controllers/SignController.php
+++ dinar/app/Http/Controllers/SignController.php
@@ -7,19 +7,14 @@ use Illuminate\Http\Request;
 class SignController extends Controller
 {
     //
-    public function signin($valor1, $valor2, $valor3, $valor4) {
-        $textFinal = "";
-        $textFinal .= ($valor1 == "") ? ' Iniciar ': $valor1 . " ";
-        $textFinal .= ($valor2 == "") ? ' sessio ': $valor2 . " ";
-        $textFinal .= ($valor3 == "") ? ' de ': $valor3 . " ";
-        $textFinal .= ($valor4 == "") ? ' l\'usuari': $valor4;
-        return view('signin') ->with('textFinal',$textFinal);
+    public function signin() {
+        return view('signin');
     }
-    public function signup($valor1, $valor2, $valor3) {
-        $textFinal = "";
-        $textFinal .= ($valor1 == "") ? ' Creacio ': $valor1 . " ";
-        $textFinal .= ($valor2 == "") ? ' d\'usuari ': $valor2 . " ";
-        $textFinal .= ($valor3 == "") ? ' nou ': $valor3 . " ";
-        return view('signup') ->with('textFinal', $textFinal);
+    public function signup() {
+        return view('signup');
+    }
+    public function products(Request $request){
+        $text = $request['product'];
+        return view('products')->with(['result'=>$text]);
     }
 }
