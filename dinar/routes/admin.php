a1516f6fa217989e5e1851fc79810e3b0f7cf09c
DinarKhazimullinITICBcn <146452935+DinarKhazimullinITICBcn@users.noreply.github.com>
2023-11-24 16:42:28 +0100

    Creat admin.php amb dues rutes, consultes i usuaris.php

---
+++ dinar/routes/admin.php
@@ -0,0 +1,9 @@
+<?php
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminController;
+
+    Route::middleware(['admin_db'])->group(Function(){
+        Route::get('/centres', [AdminController::class, 'centres']);
+        Route::post('/usuaris', [AdminController::class, 'usuaris'])->name('usuaris');
+    });
+?>
