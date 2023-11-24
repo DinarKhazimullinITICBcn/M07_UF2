3bf16e2f002f46fe0a5bd175f3e1321d0acef250
DinarKhazimullinITICBcn <146452935+DinarKhazimullinITICBcn@users.noreply.github.com>
2023-11-24 16:37:36 +0100

    afegit metodepost per les proves de post::

--- dinar/routes/web.php
+++ dinar/routes/web.php
@@ -3,22 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SignController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
     return 'welcome';
 });
 
 Route::prefix('dinar')->group(function(){
-    Route::get('/signin/{valor1}/{valor2}/{valor3}/{valor4}', [SignController::class, 'signin']);
-    Route::get('/signup/{valor1}/{valor2}/{valor3}', [SignController::class, 'signup']);
+    Route::get('/signin', [SignController::class, 'signin'])->name('signin');
+    Route::get('/signup', [SignController::class, 'signup'])->name('signup');
+});
+Route::prefix('metodepost')->group(function(){
+    Route::post('products', [SignController::class, 'products'])->name('products');
 });
