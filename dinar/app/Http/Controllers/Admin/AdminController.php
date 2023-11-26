131f6e89ec39c386566063f8765b7d39add8cfb2
DinarKhazimullinITICBcn <146452935+DinarKhazimullinITICBcn@users.noreply.github.com>
2023-11-26 17:24:21 +0100

    Afegit les funcionalitats d'array i enviament a la view corresponent

--- dinar/app/Http/Controllers/Admin/AdminController.php
+++ dinar/app/Http/Controllers/Admin/AdminController.php
@@ -18,13 +18,29 @@ class AdminController extends Controller
         return redirect('/dinar/signin');
     }
     public function centres() {
-        return view('admin.centres');
+        $centres = array(
+            array("id" => 1, "name" => "Instritut TIC de Barcelona", "address" => "C/ de Sancho de Avila, 133", "cp" => "08018", "city" => "Barcelona"),
+            array("id" => 2, "name" => "Jaume Balmes", "address" => "C/ Diputacio, 150", "cp" => "08013",  "city" => "Barcelona")
+        );
+        return view('admin.centres')->with(['centres'=>$centres]);
     }
     public function alumnat() {
-        return view('admin.alumant');
+        $alumnes = array(
+            array("id" => 1, "name" => "Carles", "surname" => "Canals", "rol" => "Alumne", "email" => "roger@roger.com"),
+            array("id" => 2, "name" => "Sofia", "surname" => "Taraco", "rol" => "Alumne", "email" => "sara@sara.com"),
+            array("id" => 3, "name" => "Francesc", "surname" => "Riugal", "rol" => "Alumne", "email" => "pere@pere.com"),
+            array("id" => 4, "name" => "Laia", "surname" => "Manresa", "rol" => "Alumne", "email" => "sara@sara.com")
+        );
+        return view('admin.alumnat')->with(['alumnes'=>$alumnes]);
     }
     public function professorat() {
-        return view('admin.professorat');
+        $professors = array(
+            array("id" => 1, "name" => "Roger", "surname" => "Sobrino", "rol" => "Professor", "email" => "roger@roger.com"),
+            array("id" => 2, "name" => "Sara", "surname" => "Gonzalez", "rol" => "Professora", "email" => "sara@sara.com"),
+            array("id" => 3, "name" => "Pere", "surname" => "Guitart", "rol" => "Professor", "email" => "pere@pere.com"),
+            array("id" => 4, "name" => "Oriol", "surname" => "Xisco", "rol" => "Professor", "email" => "sara@sara.com")
+        );
+        return view('admin.professorat')->with(['professors'=>$professors]);
     }
     public function adminVista(){
         return view('admin.admin_view');
