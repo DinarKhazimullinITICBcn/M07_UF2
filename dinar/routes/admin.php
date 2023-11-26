e30f709e6054d414282eab059b1e3fdb162ebf11
DinarKhazimullinITICBcn <146452935+DinarKhazimullinITICBcn@users.noreply.github.com>
2023-11-26 17:39:29 +0100

    Modificat les rutes per a la prova del video

--- dinar/routes/admin.php
+++ dinar/routes/admin.php
@@ -3,7 +3,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\AdminController;
 
     Route::middleware(['admin_db'])->group(Function(){
-        Route::get('/centres', [AdminController::class, 'centres']);
-        Route::post('/usuaris', [AdminController::class, 'usuaris'])->name('usuaris');
+        Route::post('/usuarisfaaf', [AdminController::class, 'usuaris'])->name('usuaris');
+        Route::get('/centresfaaf', [AdminController::class, 'centres'])->name('centres');
+        Route::get('/alumnatfaaf', [AdminController::class, 'alumnat'])->name('alumnat');
+        Route::get('/professoratfaaf', [AdminController::class, 'professorat'])->name('professorat');
+        Route::get('/adminVistafaaf', [AdminController::class, 'adminVista'])->name('adminVista');
     });
 ?>
