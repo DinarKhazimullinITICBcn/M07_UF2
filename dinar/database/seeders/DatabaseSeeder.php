b7f6e89e6ee583582a45a07d505436bf29302274
DinarKhazimullinITICBcn <146452935+DinarKhazimullinITICBcn@users.noreply.github.com>
2023-12-01 17:24:36 +0100

    Canviat el numero de files de informacio per al video

--- dinar/database/seeders/DatabaseSeeder.php
+++ dinar/database/seeders/DatabaseSeeder.php
@@ -15,8 +15,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        Centre::factory(30)->create();
-        Professor::factory(30)->create();
-        Alumne::factory(30)->create();
+        Centre::factory(10)->create();
+        Professor::factory(15)->create();
+        Alumne::factory(20)->create();
     }
 }
