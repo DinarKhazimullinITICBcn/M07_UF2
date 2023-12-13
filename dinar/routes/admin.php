b36a55d96f3dfa794f437258193ca47587ca0c04
DinarKhazimullinITICBcn <146452935+DinarKhazimullinITICBcn@users.noreply.github.com>
2023-12-13 19:08:17 +0100

    Afegit mes rutes a admin.php per crear usuaris

--- dinar/routes/admin.php
+++ dinar/routes/admin.php
@@ -1,11 +1,14 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\AlumnesController;
 
     Route::middleware(['admin_db'])->group(Function(){
         Route::post('/usuarisfaaf', [AdminController::class, 'usuaris'])->name('usuaris');
         Route::get('/centresfaaf', [AdminController::class, 'centres'])->name('centres');
-        Route::get('/alumnatfaaf', [AdminController::class, 'alumnat'])->name('alumnat');
+        Route::get('/alumnat', [AlumnesController::class, 'index'])->name('alumnat');
+        Route::get('/crearAlumne', [AlumnesController::class, 'create'])->name('crearAlumne'); 
+        Route::post('/inserirAlumne', [AlumnesController::class, 'store'])->name('inserirAlumne');
         Route::get('/professoratfaaf', [AdminController::class, 'professorat'])->name('professorat');
         Route::get('/adminVistafaaf', [AdminController::class, 'adminVista'])->name('adminVista');
     });
