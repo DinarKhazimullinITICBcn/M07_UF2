05443905df2e71e08d5a2ab654b193ecf1f264be
JunhongITICBCN <2023_junhong.zhu@iticbcn.cat>
2023-12-15 17:22:26 +0100

    Modificat el arxiu adminphp per poder fer un merge

--- dinar/routes/admin.php
+++ dinar/routes/admin.php
@@ -1,12 +1,24 @@
 <?php
+
+use App\Http\Controllers\Admin\ProfessoratController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\AdminController;
 
-    Route::middleware(['admin_db'])->group(Function(){
-        Route::post('/usuarisfaaf', [AdminController::class, 'usuaris'])->name('usuaris');
-        Route::get('/centresfaaf', [AdminController::class, 'centres'])->name('centres');
-        Route::get('/alumnatfaaf', [AdminController::class, 'alumnat'])->name('alumnat');
-        Route::get('/professoratfaaf', [AdminController::class, 'professorat'])->name('professorat');
-        Route::get('/adminVistafaaf', [AdminController::class, 'adminVista'])->name('adminVista');
-    });
-?>
+Route::middleware(['admin_db'])->group(function () {
+    // Rutes relacionades amb administradors
+    Route::post('/usuaris', [AdminController::class, 'usuaris'])->name('usuaris');
+    Route::get('/centres', [AdminController::class, 'centres'])->name('centres');
+    Route::get('/alumnat', [AdminController::class, 'alumnat'])->name('alumnat');
+
+    // Rutes relacionades amb el professorat
+    Route::get('/professorat', [ProfessoratController::class, 'index'])->name('professorat');
+    Route::get('/crearProfessorat', [ProfessoratController::class, 'create'])->name('crearProfessorat');
+    Route::post('/inserirProfessorat', [ProfessoratController::class, 'store'])->name('inserirProfessorat');
+    Route::get('/modificaProfessorat/{professorat}', [ProfessoratController::class, 'edit'])->name('modificaProfessorat');
+    Route::post('/actualitzarProfessorat/{professorat}', [ProfessoratController::class, 'update'])->name('actualitzarProfessorat');
+    Route::get('/eliminarProfessorat/{professorat}', [ProfessoratController::class, 'destroy'])->name('eliminarProfessorat');
+    Route::get('/mostraProfessorat/{professorat}', [ProfessoratController::class, 'show'])->name('mostraProfessorat');
+
+    // Ruta addicional per la vista d'administrador
+    Route::get('/adminView', [AdminController::class, 'adminView'])->name('adminView');
+});
