33aee445edeaf1946d183b443c6b44137427b73b
JunhongITICBCN <2023_junhong.zhu@iticbcn.cat>
2023-12-15 17:12:52 +0100

    View que crea un nou professor

--- my-project/resources/views/crearRegistre.blade.php
+++ my-project/resources/views/crearRegistre.blade.php
@@ -8,12 +8,8 @@
 </head>
 
 <body>
-    <form action='' method="post">
-        <label for="rol">Rol:</label>
-        <select name="rol">
-            <option value="alumnat">Alumnat</option>
-            <option value="professor">Professorat</option>
-        </select><br>
+     <!-- Formulari per afegir la informació del professor -->
+    <form action="{{route('inserirProfessorat')}}" method="post">
 
         <label for="name">Nom:</label>
         <input type="text" name="name" required><br>
@@ -21,14 +17,12 @@
         <label for="surname">Cognom:</label>
         <input type="text" name="surname" required><br>
 
-        <label for="password">Contrasenya:</label>
-        <input type="password" name="password" required><br>
-
         <label for="email">Email:</label>
         <input type="email" name="email" required><br>
 
         <input type="submit" value="Enviar">
     </form>
+    <a href="{{route('professorat')}}">Professorat</a>
 </body>
 
 </html>
