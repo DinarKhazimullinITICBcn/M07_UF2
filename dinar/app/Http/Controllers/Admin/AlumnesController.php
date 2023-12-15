2b2907efe21f8ab60a126ec66dccf0231cfa8481
DinarKhazimullinITICBcn <146452935+DinarKhazimullinITICBcn@users.noreply.github.com>
2023-12-15 16:27:29 +0100

    Afegit comentaris + les rutes de modificar, eliminar i mostrar alumne

--- dinar/app/Http/Controllers/Admin/AlumnesController.php
+++ dinar/app/Http/Controllers/Admin/AlumnesController.php
@@ -10,6 +10,8 @@ class AlumnesController extends Controller
 {
     /**
      * Display a listing of the resource.
+     * Aquesta funcio carga tota la informacio que tenim a la base de dades i la envia a la view amb
+     * tota la informacio junta.
      */
     public function index()
     {
@@ -20,6 +22,7 @@ class AlumnesController extends Controller
 
     /**
      * Show the form for creating a new resource.
+     * Envia a la vista per poder crear el alumne
      */
     public function create()
     {
@@ -28,6 +31,8 @@ class AlumnesController extends Controller
 
     /**
      * Store a newly created resource in storage.
+     * Espera un request(informacio enviada per un formulari post) i l'assigna a les variables
+     * de una clase nova del model Alumne.
      */
     public function store(Request $request)
     {
@@ -41,33 +46,47 @@ class AlumnesController extends Controller
 
     /**
      * Display the specified resource.
+     * Ens envia a una vista on mostra la informacio del alumne especific a la isntancia clicada.
+     * O sigui, que quan nosaltres truquem aquesta funcio, ens ve amb una instancia especifica del alumne.
      */
     public function show(Alumne $alumne)
     {
-        //
+        return view('admin.mostrarAlumne')->with(['alumne' => $alumne]);
     }
 
     /**
      * Show the form for editing the specified resource.
+     * Envia a la vista de la modificacio d'un alumne especific d'una instancia.
      */
     public function edit(Alumne $alumne)
     {
-        //
+        return view('admin.modificaAlumne')->with(['alumne' => $alumne]);
     }
 
     /**
      * Update the specified resource in storage.
+     * Actualitza la informacio de un alumne, espera el formulari post i una instancia d'alumne. Fa un
+     * update(actualitzacio) i envia a la vista general amb tota la informacio cargada.
      */
     public function update(Request $request, Alumne $alumne)
     {
-        //
+        $alumne->update([
+            'name' => $request->name,
+            'surname' => $request->surname,
+            'email' => $request->email,
+        ]);
+        $alumnes = Alumne::all();
+        return view('admin.alumnat')->with(['alumnes' => $alumnes]);
     }
 
     /**
      * Remove the specified resource from storage.
+     * Agafa una instancia especifica d'alumne i el eliminar per id.
      */
     public function destroy(Alumne $alumne)
     {
-        //
+        $alumne->delete();
+        $alumnes = Alumne::all();
+        return view('admin.alumnat')->with(['alumnes' => $alumnes]);
     }
 }
