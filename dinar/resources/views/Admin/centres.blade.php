b1e1dac7ceda26d77af81d77b7f1c0d569df5f1a
Joel Ghanem <146096638+JoelGhanemITICBCN@users.noreply.github.com>
2023-12-15 16:35:19 +0100

    Admin views funcionals

--- dinar/resources/views/Admin/centres.blade.php
+++ dinar/resources/views/Admin/centres.blade.php
@@ -3,28 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>centres</title>
+    <title>View</title>
 </head>
 <body>
-    <h1>LLISTA CENTRES</h1>
-    <table>
+  <h1>LLISTA CENTRES</h1>  
+<br>
+<table>
     <tr>
-        <th>ID</th>
-        <th>NAME</th>
+        <th>Name</th>
         <th>Address</th>
         <th>CP</th>
         <th>City</th>
     </tr>
     @foreach ($centres as $centre)
-        <tr>
-            <td>{{ $centre['id'] }}</td>
-            <td>{{ $centre['name'] }}</td>
-            <td>{{ $centre['address'] }}</td>
-            <td>{{ $centre['cp'] }}</td>
-            <td>{{ $centre['city'] }}</td>
-        </tr>
+    <tr>
+        <td>{{ $centre->name }}</td>
+        <td>{{ $centre->adress }}</td>
+        <td>{{ $centre->cp }}</td>
+        <td>{{ $centre->city }}</td>
+<td><a href="{{ route('modificaCentres',$centre) }}">Modifica</a></td>
+<td><a href="{{ route('eliminaCentres',$centre) }}">Elimina</a></td>
+    </tr>
     @endforeach
-    </table>
-    <a href="{{route('adminVista')}}">ADMIN VISTA</a>
+</table>
+    <a href="{{ route('crearCentres') }}">ADD CENTRE</a>
+    <a href="{{ route('viewGet') }}">ADMIN VISTA</a><br>
 </body>
 </html>
