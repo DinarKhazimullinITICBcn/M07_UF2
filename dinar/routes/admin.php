76f87eaf83e42ca0bf4299a592a7179fd2f0d03b
JoelGhanemITICBCN <2023_joel.ghanem@iticbcn.cat>
2023-12-15 16:46:36 +0100

    ruta admin

--- dinar/routes/admin.php
+++ dinar/routes/admin.php
@@ -6,9 +6,13 @@ use App\Http\Controllers\Admin\CentresController;
 Route::middleware(['admin_db'])->group(function(){
     Route::post('/view',[AdminController::class, 'view'])->name('view');
     Route::get('/view',[AdminController::class, 'viewGet'])->name('viewGet');
-    Route::get('/centres',[AdminController::class, 'centres'])->name('centres');
+    Route::any('/centres',[CentresController::class, 'index'])->name('centres');
     Route::get('/professorat',[AdminController::class, 'professorat'])->name('professorat');
     Route::get('/alumnat',[AdminController::class, 'alumnat'])->name('alumnat');
-    Route::get('/centres',[CentresController::class, 'centres'])->name('afegeixcentre');
+    Route::get('/crearCentres',[CentresController::class, 'create'])->name('crearCentres');
+    Route::post('/inserirCentres',[CentresController::class, 'store'])->name('inserirCentres');
+    Route::any('/eliminaCentres/{centre}',[CentresController::class, 'destroy'])->name('eliminaCentres');
+    Route::any('/modificaCentres/{centre}',[CentresController::class, 'edit'])->name('modificaCentres');
+    Route::any('/actualitzaCentres/{centre}',[CentresController::class, 'update'])->name('update');
 });
 ?>
